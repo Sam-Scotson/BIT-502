44367fc9c56415c1b67682ca262674c5867a7af8
Sam Scotson <118854225+Sam-Scotson@users.noreply.github.com>
2023-04-15 20:09:29 +1200

    Update nameList.py

--- nameList.py
+++ nameList.py
@@ -30,3 +30,26 @@ while c <3:
     hello_display()
     c = c + 1
 print("Bye for now!")
+
+#
+def people_input():
+    global name,age,vaccine
+    name = input('enter name')
+    age = int(input('enter age'))
+    vaccine = input('do you have a vaccine pass (y/n').lower
+
+
+def people_sort(name, age, vaccine):
+    if age < 18:
+        print(name+', you are not eligible to attend.')
+        import sys
+        sys.exit()
+    elif age >= 50 and vaccine == 'y':
+        print(name+', go to hall A / Category 1')
+    elif age >= 50 and vaccine == 'n':
+        print(name+', go to hall B / Category 2')
+    else:
+        print(name+', go to hall C / Category 3')
+
+people_input()
+people_sort(name, age, vaccine)
